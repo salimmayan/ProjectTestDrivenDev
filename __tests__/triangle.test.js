41db16a10004ab4dd14fe1cfda3123fc7abb8003
Student <example@example.com>
2020-02-25 21:01:37 -0800

    Added additional tests and converted instantiated object to reusable

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,16 +1,38 @@
 import { Triangle } from './../src/frontend.js';
 
 describe('Triangle', () => {
+    var reusableTriangle;
 
-  test('should correctly create a triangle object with three lengths', () => {
-    var triangle = new Triangle(2,4,5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
-  });
+    beforeEach(() => {
+        reusableTriangle = new Triangle(5, 5, 5);
+      });
 
-  test('should correctly determine whether 3 lengths can be made into a triangle', () => {
-      var notTriangle = new Triangle(3,9,22);
-      expect(notTriangle.checkType()).toEqual("not a triangle");
-  });
+      test('should show how beforeEach() works', () => {
+        console.log(reusableTriangle);
+      });
+
+    test('should correctly create a triangle object with three lengths', () => {
+        reusableTriangle = new Triangle(2, 4, 5);
+        expect(reusableTriangle.side1).toEqual(2);
+        expect(reusableTriangle.side2).toEqual(4);
+        expect(reusableTriangle.side3).toEqual(5);
+    });
+
+
+
+    test('should correctly determine whether 3 lengths can be made into a triangle', () => {
+        reusableTriangle = new Triangle(3, 9, 22);
+        expect(reusableTriangle.checkType()).toEqual("not a triangle");
+    });
+
+    test('should correctly determine whether three lengths make a scalene triangle', function () {
+        reusableTriangle = new Triangle(4, 5, 7)
+        expect(reusableTriangle.checkType()).toEqual("scalene triangle");
+    });
+
+    test('should correctly determine whether three lengths make an isosceles triangle', () => {
+        reusableTriangle = new Triangle(5,5,7)
+        expect(reusableTriangle.checkType()).toEqual("isosceles triangle");
+      });
+
 });
