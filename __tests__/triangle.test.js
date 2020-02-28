4deff62fd012c296c376f423f6809058d4e58986
Student <example@example.com>
2020-02-27 20:39:21 -0800

    Added a Character test

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,20 +1,40 @@
-import { Warrior } from './../src/frontend.js';
+import { Warrior, Character, Enemy } from './../src/frontend.js';
 
+// Warrior Suite
 describe('Warrior', () => {
   var reuseableWarrior;
 
   beforeEach(() => {
-    reuseableWarrior = new Warrior("Dummy", "Pinata", 1);
-    expect(reuseableWarrior.name).toEqual("Dummy");
-    expect(reuseableWarrior.weapon).toEqual("Pinata");
-    expect(reuseableWarrior.level).toEqual(1);
+    reuseableWarrior = new Warrior("Arthur", "Bow");
+    expect(reuseableWarrior.name).toEqual("Arthur");
+    expect(reuseableWarrior.weapon).toEqual("Bow");
   });
 
+  // Warrior Test
   test('should correctly create a warrior object with a name, a weapon and a level', () => {
-    reuseableWarrior = new Warrior("Michael", "Axe", 10);
+    reuseableWarrior = new Warrior("Michael", "Axe");
     expect(reuseableWarrior.name).toEqual("Michael");
     expect(reuseableWarrior.weapon).toEqual("Axe");
-    expect(reuseableWarrior.level).toEqual(10);
   });
 
+});
+
+// Character Suite
+describe('Character', () => {
+  var reuseableCharacter;
+
+  beforeEach(() => {
+    reuseableCharacter = new Character("Dummy", "Lance", 10);
+    expect(reuseableCharacter.name = "Dummy");
+    expect(reuseableCharacter.weapon = "Lance");
+    expect(reuseableCharacter.level = 10);
+  });
+
+  // Character Test
+  test('should correctly create a character object with a name, a weapon, and a level', () => {
+    reuseableCharacter = new Character("NPC", "Basic Sword", 1);
+    expect(reuseableCharacter.name = "NPC");
+    expect(reuseableCharacter.weapon = "Basic Sword");
+    expect(reuseableCharacter.level = 1);
+  });
 });
