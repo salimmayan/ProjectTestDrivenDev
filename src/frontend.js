795490b3ca826a201d9d14b07d423aa01d163a63
Student <example@example.com>
2020-02-27 20:40:23 -0800

    Add Character class that is extended by Warrior and Enemy

--- src/frontend.js
+++ src/frontend.js
@@ -1,12 +1,34 @@
 const characters = [];
 
-export class Warrior {
-    constructor(name, weapon, level) {
-        this.name = name;
-        this.weapon = weapon;
-        this.level = level;
-        let strength = 5;
-        let health = 10;
-    }
+
+export class Character {
+  constructor(name="NPC", weapon="Basic Sword", level=1, strength=5, health=20) {
+    this.name = name;
+    this.weapon = weapon;
+    this.level = level;
+    this.strength = strength;
+    this.health = health;
+    // this.currentId = currentId ++;
+  }
 }
+
+export class Warrior extends Character {
+  constructor(name, weapon) {
+    super();
+    this.name = name;
+    this.weapon = weapon;
+  }  
+}
+
+export class Enemy extends Character {
+  constructor(hostility) {
+    super();
+    this.hostility = hostility;
+  }
+}
+
+const computerNpcs = new Enemy(5);
+const usersWarrior = new Warrior("Michael", "Axe");
+console.log(computerNpcs);
+console.log(usersWarrior);
 
